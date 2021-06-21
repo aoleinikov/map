c06e626b59c5e45d50f4c440635f55f31586a03f
aoleinikov <a.oleinikov@atilog.org>
2021-06-21 18:11:37 +0300

    added mobile logic

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+
+function mapInit() {
+  const isTabletOrMore = window.innerWidth > 767;
+
+  if (!isTabletOrMore) {
+    return;
+  }
+
   const world = document.querySelector('.world-wrapper');
   const countries = world.querySelectorAll('.country');
-
-  console.log(countries)
+  const buttons = world.querySelectorAll('.symbol')
 
   countries.forEach(country => {
     country.addEventListener('mouseover', (e) => {
@@ -11,9 +18,26 @@
 
     country.addEventListener('mouseout', (e) => {
       toggleShowLineToCountry(country, false)
-      changeCountryFill(country, )  })
+      changeCountryFill(country,)
+    })
   })
 
+  buttons.forEach(button => {
+
+    button.addEventListener('click', (e) => {
+      const id = button.classList[1];
+      const activeCls = 'activeClick';
+      const activeClsLine = 'activeLineClick';
+      const line = world.querySelectorAll('.to' + id)
+      let isActive = false;
+      world.querySelectorAll('.compassionate' + id).forEach(el => {
+        isActive = [...el.classList].includes(activeCls)
+        isActive ? el.classList.remove(activeCls) : el.classList.add(activeCls)
+      })
+      console.log(line)
+      line.forEach(l => isActive ? l.classList.remove(activeClsLine) : l.classList.add(activeClsLine));
+    })
+  })
 
   function changeCountryFill(country, color = '') {
     const place = country.querySelectorAll('path')
@@ -26,8 +50,14 @@
 
   function toggleShowLineToCountry(country, isShowing) {
     const id = country.id;
-    const line = world.querySelectorAll('.to'+id)
+    const line = world.querySelectorAll('.to' + id)
+    const info = world.querySelectorAll('.compassionate' + id)
     if (!line.length) return
     line.forEach(l => l.style.opacity = isShowing ? 1 : '');
+    info.forEach(item => {
+      item.classList[isShowing ? "add" : "remove"]("active")
+    });
   }
+}
 
+mapInit()
